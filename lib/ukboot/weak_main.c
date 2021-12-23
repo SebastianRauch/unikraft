a9f87a14044555d3fdab39f6d70c83506e021bdf
Sebastian Rauch <s.rauch94@gmail.com>
2021-12-23 19:10:56 +0100

    Fix weak main for non-EPT backends.
    Signed-off-by: Sebastian Rauch <s.rauch94@gmail.com>

--- lib/ukboot/weak_main.c
+++ lib/ukboot/weak_main.c
@@ -1,12 +1,19 @@
 #include <errno.h>
 #include <uk/essentials.h>
+#include <uk/sched.h>
 #include <uk/print.h>
 #include <flexos/isolation.h>
 
 /* Internal main */
 int __weak main(int argc __unused, char *argv[] __unused)
 {
+#if CONFIG_LIBFLEXOS_VMEPT
 	uk_pr_info("weak main() called. Executing RPC server.\n");
 	flexos_vmept_rpc_server_loop(NULL);
 	return -EINVAL;
+#else
+	while (1) {
+		uk_sched_yield();
+	}
+#endif /* CONFIG_LIBFLEXOS_VMEPT */
 }
